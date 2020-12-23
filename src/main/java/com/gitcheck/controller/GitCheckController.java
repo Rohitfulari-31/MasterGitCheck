25765a41a9eb8628055cd79adec813a0eac06f14
rohitfulari-31 <rohitfulari31@gmail.com>
2020-12-23 23:36:37 +0530

    added

--- src/main/java/com/gitcheck/controller/GitCheckController.java
+++ src/main/java/com/gitcheck/controller/GitCheckController.java
@@ -5,5 +5,9 @@ public class GitCheckController {
 	public void m1(){
 		System.out.println("M1 method for git");
 	}
+	
+	public void m2(){
+		System.out.println("m2 method for sub branch one");
+	}
 
 }
